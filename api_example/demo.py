5b5cb5096bc3769bfd888c88f8ad424cbfc1d392
awkrail <taichitary@gmail.com>
2024-09-06 18:24:20 +0900

    fix mypy puzzle

--- api_example/demo.py
+++ api_example/demo.py
@@ -15,41 +15,32 @@ under the License.
 """
 import os
 import subprocess
-import pprint
 import torch
 from lighthouse.models import CGDETRPredictor
+from typing import Dict, List, Optional
+
+def load_weights(weight_dir: str) -> None:
+    if not os.path.exists(os.path.join(weight_dir, 'clip_cg_detr_qvhighlight.ckpt')):  
+        command = 'wget -P gradio_demo/weights/ https://zenodo.org/records/13363606/files/clip_cg_detr_qvhighlight.ckpt'
+        subprocess.run(command, shell=True)
+
+    if not os.path.exists('SLOWFAST_8x8_R50.pkl'):
+        subprocess.run('wget https://dl.fbaipublicfiles.com/pyslowfast/model_zoo/kinetics400/SLOWFAST_8x8_R50.pkl', shell=True)
+
+    if not os.path.exists('Cnn14_mAP=0.431.pth'):
+        subprocess.run('wget https://zenodo.org/record/3987831/files/Cnn14_mAP%3D0.431.pth', shell=True)
 
 # use GPU if available
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-weight_dir = 'gradio_demo/weights'
-if not os.path.exists(os.path.join(weight_dir, 'clip_cg_detr_qvhighlight.ckpt')):  
-    command = 'wget -P gradio_demo/weights/ https://zenodo.org/records/13363606/files/clip_cg_detr_qvhighlight.ckpt'
-    subprocess.run(command, shell=True)
-
-if not os.path.exists('SLOWFAST_8x8_R50.pkl'):
-    subprocess.run('wget https://dl.fbaipublicfiles.com/pyslowfast/model_zoo/kinetics400/SLOWFAST_8x8_R50.pkl', shell=True)
-
-if not os.path.exists('Cnn14_mAP=0.431.pth'):
-    subprocess.run('wget https://zenodo.org/record/3987831/files/Cnn14_mAP%3D0.431.pth', shell=True)
-
-weight_path = os.path.join(weight_dir, 'clip_cg_detr_qvhighlight.ckpt')
-model = CGDETRPredictor(weight_path, device=device, feature_name='clip', slowfast_path=None, pann_path=None)
-
-"""
-# slowfast_path is necesary if you use clip_slowfast features
-weight_path = os.path.join(weight_dir, 'clip_slowfast_cg_detr_qvhighlight.ckpt')
-model = CGDETRPredictor(weight_path, device=device, feature_name='clip_slowfast', slowfast_path='SLOWFAST_8x8_R50.pkl', pann_path=None)
-
-# slowfast_path and pann_path are necesary if you use clip_slowfast_pann features
-weight_path = os.path.join(weight_dir, 'clip_slowfast_cg_detr_qvhighlight.ckpt')
-model = CGDETRPredictor(weight_path, device=device, feature_name='clip_slowfast_pann', slowfast_path='SLOWFAST_8x8_R50.pkl', pann_path='Cnn14_mAP=0.431.pth')
-"""
+device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
+weight_dir: str = 'gradio_demo/weights'
+weight_path: str = os.path.join(weight_dir, 'clip_cg_detr_qvhighlight.ckpt')
+model: CGDETRPredictor = CGDETRPredictor(weight_path, device=device, feature_name='clip', 
+                                        slowfast_path=None, pann_path=None)
 
 # encode video features
 model.encode_video('api_example/RoripwjYFp8_60.0_210.0.mp4')
 
 # moment retrieval & highlight detection
-query = 'A woman wearing a glass is speaking in front of the camera'
-prediction = model.predict(query)
-pprint.pp(prediction)
+query: str = 'A woman wearing a glass is speaking in front of the camera'
+prediction: Optional[Dict[str, List[float]]] = model.predict(query)
+print(prediction)
--- lighthouse/models.py
+++ lighthouse/models.py
@@ -46,80 +46,128 @@ from lighthouse.common.uvcom import build_model as build_model_uvcom
 from lighthouse.common.tr_detr import build_model as build_model_tr_detr
 from lighthouse.common.taskweave import build_model as build_model_task_weave
 
+from lighthouse.common.qd_detr import QDDETR
+from lighthouse.common.moment_detr import MomentDETR
+from lighthouse.common.cg_detr import CGDETR
+from lighthouse.common.eatr import EaTR
+from lighthouse.common.uvcom import UVCOM
+from lighthouse.common.tr_detr import TRDETR
+from lighthouse.common.taskweave import TaskWeave
+
 from lighthouse.common.utils.span_utils import span_cxw_to_xx
 from lighthouse.feature_extractor import VideoFeatureExtractor
 
+from typing import Optional, Union, Mapping, Any, Dict, List, Tuple
 
 class BasePredictor:
-    def __init__(self, model_name, ckpt_path, device, feature_name, slowfast_path=None, pann_path=None):
-        ckpt = torch.load(ckpt_path, map_location="cpu")
-        args = ckpt["opt"]
-        self.clip_len = args.clip_length
-        self.device = device
+    def __init__(
+        self,
+        model_name: str,
+        ckpt_path: str,
+        device: str,
+        feature_name: str,
+        slowfast_path: Optional[str] = None,
+        pann_path: Optional[str] = None) -> None:
+        
+        ckpt = torch.load(ckpt_path, map_location='cpu')
+        args = ckpt['opt']
+        self.clip_len: float = args.clip_length
+        self.device: str = device
         args.device = device # for CPU users
         self.feature_extractor = VideoFeatureExtractor(
             framerate=1/self.clip_len, size=224, centercrop=(feature_name != 'resnet_glove'),
             feature_name=feature_name, device=device, slowfast_path=slowfast_path, pann_path=pann_path,
         )
+        self.model: Union[MomentDETR, QDDETR, EaTR, UVCOM, TaskWeave, CGDETR, TRDETR] = self.initialize_model(args, model_name)
+        self.load_weights(ckpt['model'])
+        self.feature_name: str = feature_name
+        self.model_name: str = model_name
+        self.video_feats: Optional[torch.Tensor] = None
+        self.video_mask: Optional[torch.Tensor] = None
+        self.video_path: Optional[str] = None
+        self.audio_feats: Optional[torch.Tensor] = None
+
+    def initialize_model(
+        self,
+        args: Dict[str, Union[str, float, int, List[str]]],
+        model_name: str) -> Union[MomentDETR, QDDETR, EaTR, UVCOM, TaskWeave, CGDETR, TRDETR]:
+        
         if model_name == 'moment_detr':
-            self.model, _ = build_model_moment_detr(args)
+            model, _ = build_model_moment_detr(args)
         elif model_name == 'qd_detr':
-            self.model, _ = build_model_qd_detr(args)
+            model, _ = build_model_qd_detr(args)
         elif model_name == 'eatr':
-            self.model, _ = build_model_eatr(args)
+            model, _ = build_model_eatr(args)
         elif model_name == 'cg_detr':
-            self.model, _ = build_model_cg_detr(args)
+            model, _ = build_model_cg_detr(args)
         elif model_name == 'tr_detr':
-            self.model, _ = build_model_tr_detr(args)
+            model, _ = build_model_tr_detr(args)
         elif model_name == 'uvcom':
-            self.model, _ = build_model_uvcom(args)
+            model, _ = build_model_uvcom(args)
         elif model_name == 'taskweave':
-            self.model, _ = build_model_task_weave(args)
+            model, _ = build_model_task_weave(args)
         else:
             raise NotImplementedError
+
+        return model
+
+    def load_weights(
+        self, 
+        model_weight: Mapping[str, Any]) -> None:
         
-        self.feature_name = feature_name
-        self.model_name = model_name
-        self.model.load_state_dict(ckpt["model"])
+        self.model.load_state_dict(model_weight)
         self.model.to(self.device)
         self.model.eval()
-        self.video_feats = None
-        self.video_mask = None
-        self.video_path = None
-        self.audio_feats = None
 
-    @torch.no_grad()
-    def encode_video(self, video_path):
-        # construct model inputs
-        video_feats = self.feature_extractor.encode_video(video_path)
-        video_feats = F.normalize(video_feats, dim=-1, eps=1e-5)
-        n_frames = len(video_feats)
+    def normalize_and_concat_with_timestamps(
+        self,
+        video_feats: torch.Tensor) -> torch.Tensor:
+        normalized_video_feats = F.normalize(video_feats, dim=-1, eps=1e-5)
+        n_frames = len(normalized_video_feats)
         tef_st = torch.arange(0, n_frames, 1.0) / n_frames
         tef_ed = tef_st + 1.0 / n_frames
         tef = torch.stack([tef_st, tef_ed], dim=1).to(self.device)
-        video_feats = torch.cat([video_feats, tef], dim=1)
-        assert n_frames <= 75, "The positional embedding only support video up to 150 secs (i.e., 75 2-sec clips) in length"
+        timestamped_video_feats = torch.cat([video_feats, tef], dim=1)
+        return timestamped_video_feats
+
+    @torch.no_grad()
+    def encode_video(
+        self,
+        video_path: str) -> None:
+        video_feats: torch.Tensor = self.feature_extractor.encode_video(video_path)
+        timestamed_video_feats: torch.Tensor = self.normalize_and_concat_with_timestamps(video_feats)
+        n_frames: int = len(timestamed_video_feats)
+
+        if n_frames > 75:
+            raise ValueError('The positional embedding only support video up to 150 secs (i.e., 75 2-sec clips) in length')
         
-        video_feats = video_feats.unsqueeze(0)
-        video_mask = torch.ones(1, n_frames).to(self.device)
-        self.video_feats = video_feats
+        timestamed_video_feats = timestamed_video_feats.unsqueeze(0)
+        video_mask: torch.Tensor = torch.ones(1, n_frames).to(self.device)
+        self.video_feats = timestamed_video_feats
         self.video_mask = video_mask
         self.video_path = video_path
         if self.feature_name == 'clip_slowfast_pann':
-            self.audio_feats = self.feature_extractor.encode_audio(video_path)
+            self.audio_feats = self.encode_audio(video_path)
+    
+    def encode_audio(
+        self,
+        video_path: str) -> torch.Tensor:
+        return self.feature_extractor.encode_audio(video_path)
 
-    @torch.no_grad()
-    def predict(self, query):
+    def is_predictable(
+        self,
+        ) -> bool:
         if self.video_feats is None or self.video_mask is None or self.video_path is None:
-            return None
-
+            return False
         if self.feature_name == 'clip_slowfast_pann' and self.audio_feats is None:
-            return None
+            return False
+        return True
 
-        query_feats, query_mask = self.feature_extractor.encode_text(query)  # #text * (L, d) -> CLIP or GloVe
-        if self.feature_name != "resnet_glove":
-            query_feats = F.normalize(query_feats, dim=-1, eps=1e-5)
-
+    def prepare_batch(
+        self,
+        query_feats: torch.Tensor,
+        query_mask: torch.Tensor) -> Dict[str, Optional[torch.Tensor]]:
+        
         if self.model_name == 'cg_detr':
             model_inputs = dict(
                 src_vid=self.video_feats,
@@ -137,63 +185,154 @@ class BasePredictor:
                 src_txt_mask=query_mask,
                 src_aud=self.audio_feats
             )
-
-        # decode outputs
+        
         if self.model_name == 'taskweave':
             model_inputs["epoch_i"] = None
-            outputs, _ = self.model(**model_inputs)
-        else:
-            outputs = self.model(**model_inputs)
+
+        return model_inputs
+
+    def post_processing(
+        self,
+        inputs: Dict[str, Optional[torch.Tensor]],
+        outputs: Dict[str, torch.Tensor],
+    ) -> Tuple[List[float], List[float]]:
         prob = F.softmax(outputs["pred_logits"], -1).squeeze(0).cpu()
         scores = prob[:,0]
         pred_spans = outputs["pred_spans"].squeeze(0).cpu()
-        saliency_scores = outputs["saliency_scores"][model_inputs["src_vid_mask"] == 1].cpu().tolist()
+        
+        if self.video_feats is not None:
+            video_duration = self.video_feats.shape[1] * self.clip_len
+            pred_spans = torch.clamp(span_cxw_to_xx(pred_spans) * video_duration, min=0, max=video_duration)
+            cur_ranked_preds = torch.cat([pred_spans, scores[:, None]], dim=1).tolist()
+            cur_ranked_preds = sorted(cur_ranked_preds, key=lambda x: x[2], reverse=True)
+            cur_ranked_preds = [[float(f"{e:.4f}") for e in row] for row in cur_ranked_preds]
+            saliency_scores = outputs["saliency_scores"][inputs["src_vid_mask"] == 1].cpu().tolist()
+        else:
+            cur_ranked_preds = []
+            saliency_scores = []
 
-        # compose prediction
-        video_duration = self.video_feats.shape[1] * self.clip_len
-        pred_spans = span_cxw_to_xx(pred_spans) * video_duration
-        pred_spans = torch.clamp(pred_spans, min=0, max=video_duration)
-        cur_ranked_preds = torch.cat([pred_spans, scores[:, None]], dim=1).tolist()
-        cur_ranked_preds = sorted(cur_ranked_preds, key=lambda x: x[2], reverse=True)
-        cur_ranked_preds = [[float(f"{e:.4f}") for e in row] for row in cur_ranked_preds]
-        prediction = {
-            "query": query,
-            "pred_relevant_windows": cur_ranked_preds,
-            "pred_saliency_scores": saliency_scores,
-        }
-        return prediction
+        return cur_ranked_preds, saliency_scores
+
+    @torch.no_grad()
+    def predict(
+        self,
+        query: str) -> Optional[Dict[str, List[float]]]:
+        is_predictable = self.is_predictable()
+        if is_predictable:
+            # extract text feature
+            query_feats: torch.Tensor
+            query_mask: torch.Tensor
+            query_feats, query_mask = self.feature_extractor.encode_text(query)            
+            if self.feature_name != "resnet_glove":
+                query_feats = F.normalize(query_feats, dim=-1, eps=1e-5)
+
+            # forward inputs to the model - taskweave returns a tuple of prediction and losses
+            inputs = self.prepare_batch(query_feats, query_mask)
+            if self.model_name == 'taskweave':
+                outputs, _ = self.model(**inputs)
+            else:
+                outputs = self.model(**inputs)
+
+            ranked_moments, saliency_scores = self.post_processing(inputs, outputs)
+
+            if len(ranked_moments) == 0 and len(ranked_moments) == 0:
+                return None
+            else:
+                prediction = {
+                    "pred_relevant_windows": ranked_moments,
+                    "pred_saliency_scores": saliency_scores,
+                }
+                return prediction
+        else:
+            return None
 
 
 class MomentDETRPredictor(BasePredictor):
-    def __init__(self, ckpt_path, device, feature_name, slowfast_path=None, pann_path=None):
-        super().__init__('moment_detr', ckpt_path, device, feature_name, slowfast_path, pann_path)
+    def __init__(
+        self, 
+        ckpt_path: str,
+        device: str,
+        feature_name: str,
+        slowfast_path: Optional[str] = None,
+        pann_path: Optional[str] = None
+        ) -> None:
+        super().__init__('moment_detr', ckpt_path, device,
+                        feature_name, slowfast_path, pann_path)
 
 
 class QDDETRPredictor(BasePredictor):
-    def __init__(self, ckpt_path, device, feature_name, slowfast_path=None, pann_path=None):
-        super().__init__('qd_detr', ckpt_path, device, feature_name, slowfast_path, pann_path)
+    def __init__(
+        self, 
+        ckpt_path: str,
+        device: str,
+        feature_name: str,
+        slowfast_path: Optional[str] = None,
+        pann_path: Optional[str] = None
+        ) -> None:
+        super().__init__('qd_detr', ckpt_path, device,
+                        feature_name, slowfast_path, pann_path)
 
 
 class EaTRPredictor(BasePredictor):
-    def __init__(self, ckpt_path, device, feature_name, slowfast_path=None, pann_path=None):
-        super().__init__('eatr', ckpt_path, device, feature_name, slowfast_path, pann_path)
+    def __init__(
+        self, 
+        ckpt_path: str,
+        device: str,
+        feature_name: str,
+        slowfast_path: Optional[str] = None,
+        pann_path: Optional[str] = None
+        ) -> None:
+        super().__init__('eatr', ckpt_path, device,
+                         feature_name, slowfast_path, pann_path)
 
 
 class CGDETRPredictor(BasePredictor):
-    def __init__(self, ckpt_path, device, feature_name, slowfast_path=None, pann_path=None):
-        super().__init__('cg_detr', ckpt_path, device, feature_name, slowfast_path, pann_path)
+    def __init__(
+        self, 
+        ckpt_path: str,
+        device: str,
+        feature_name: str,
+        slowfast_path: Optional[str] = None,
+        pann_path: Optional[str] = None
+        ) -> None:
+        super().__init__('cg_detr', ckpt_path, device, 
+                         feature_name, slowfast_path, pann_path)
 
 
 class TRDETRPredictor(BasePredictor):
-    def __init__(self, ckpt_path, device, feature_name, slowfast_path=None, pann_path=None):
-        super().__init__('tr_detr', ckpt_path, device, feature_name, slowfast_path, pann_path)
+    def __init__(
+        self, 
+        ckpt_path: str,
+        device: str,
+        feature_name: str,
+        slowfast_path: Optional[str] = None,
+        pann_path: Optional[str] = None
+        ) -> None:
+        super().__init__('tr_detr', ckpt_path, device,
+                         feature_name, slowfast_path, pann_path)
 
 
 class UVCOMPredictor(BasePredictor):
-    def __init__(self, ckpt_path, device, feature_name, slowfast_path=None, pann_path=None):
-        super().__init__('uvcom', ckpt_path, device, feature_name, slowfast_path, pann_path)
+    def __init__(
+        self, 
+        ckpt_path: str,
+        device: str,
+        feature_name: str,
+        slowfast_path: Optional[str] = None,
+        pann_path: Optional[str] = None
+        ) -> None:
+        super().__init__('uvcom', ckpt_path, device,
+                         feature_name, slowfast_path, pann_path)
 
 
 class TaskWeavePredictor(BasePredictor):
-    def __init__(self, ckpt_path, device, feature_name, slowfast_path=None, pann_path=None):
-        super().__init__('taskweave', ckpt_path, device, feature_name, slowfast_path, pann_path)
+    def __init__(
+        self, 
+        ckpt_path: str,
+        device: str,
+        feature_name: str,
+        slowfast_path: Optional[str] = None,
+        pann_path: Optional[str] = None
+        ) -> None:
+        super().__init__('taskweave', ckpt_path, device,
+                         feature_name, slowfast_path, pann_path)
